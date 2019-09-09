c47f4529e0fc2207c5a0e79fdebecf576195a574
shayho <shayho>
2019-09-09 17:26:57 +0300

    Getting ready to upload to pypi - v0.1-alpha

--- setup.py
+++ setup.py
@@ -8,11 +8,25 @@ def readme():
 
 setuptools.setup(
     name='pymodule',
-    version='0.1',
+    version='0.1-alpha',
+    license='GPL-3.0',
     description='Python module interface',
     long_description=readme(),
-    url='https://github.com/Fleishman-Lab/flab',
+    url='https://github.com/hochshi/pymodule',
+    download_url=
+    'https://github.com/hochshi/pymodule/archive/v0.1-alpha.tar.gz',
+    keywords=['RHEL', 'module'],
     author='Shi Hoch',
     author_email='hochshi@gmail.com',
     packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: OS Integration :: RHEL Module',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
 )
